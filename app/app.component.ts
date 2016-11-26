83802788fa72e782a7d613c6c040d487ab0e9aee
Aviral Mansingka <aviralmansingka@gmail.com>
2016-11-25 21:20:00 -0500

    Added users to AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,37 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div class="jumbotron">
-            <h1>Welcome to our App!</h1>
-        </div>
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="/" class="navbar-brand">My Angular 2 App!</a>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+
+            <div class="jumbotron">
+                <h1>Welcome to our App!</h1>
+            </div>
+
+            <div *ngIf="users">
+                <div *ngFor="let user of users">
+                    <p>{{ user.name }} ({{ user.username }})</p>
+                </div>
+            </div>
+        </main>
+
     `,
     styles: [`
         .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
     `]
 })
-export class AppComponent {}
+export class AppComponent {
+    message = 'Hello!';
+    users = [
+        { id: 25, name: 'Aviral Mansingka', username: 'aviralmansingka' },
+        { id: 26, name: 'Mani Kumar', username: 'manikumar' },
+        { id: 27, name: 'Nihanshu Purohit', username: 'nihanshu' }
+    ];
+}
